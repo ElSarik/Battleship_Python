6678ebe4dda68263e80cd1d61274cd54af7ea4ad
ElSarik <sarikakisilias@gmail.com>
2022-03-20 12:49:23 +0200

    Added grid initialization function
    - Initializes an array of size 10 (default), with each element being the grid coordinates in format LetterNumber (A1, D6, etc.)

---
+++ grid.py
@@ -0,0 +1,15 @@
+import numpy as np
+import string
+
+def grid_init():
+    global grid
+
+    size = 10
+
+    letters = list(string.ascii_uppercase)
+    grid = []
+
+    for row in range(size):
+        grid.append([letters[row] + str(column) for column in range(size)])
+
+
