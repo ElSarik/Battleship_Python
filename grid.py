6715f0bbcec79a2d3929b47cbd10dd4f6a4c8337
ElSarik <sarikakisilias@gmail.com>
2022-03-22 13:58:41 +0200

    Changed grid to np.array after initialization

--- grid.py
+++ grid.py
@@ -1,19 +1,24 @@
 import numpy as np
 import string
 
+from calculations import reverse_variables
+
 def grid_init():
     global grid
 
     size = 10
 
     letters = list(string.ascii_uppercase)
-    grid = []
+    list_grid = []
+    
 
     for row in range(size):
-        grid.append([letters[row] + str(column) for column in range(size)])
+        list_grid.append([letters[row] + str(column) for column in range(size)])
+
+    grid = np.array(list_grid)
 
 
 def grid_display():
-    print(np.matrix(grid))
+    print(grid)
 
 
