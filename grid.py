adb274537e3fe15bf44ab6babdd290cec6035943
ElSarik <sarikakisilias@gmail.com>
2022-03-22 14:02:21 +0200

    Created ship_placement function
    - Replaces specified grid cells with a ship (##).
    - Contains input correction and validation.

--- grid.py
+++ grid.py
@@ -22,3 +22,42 @@ def grid_display():
     print(grid)
 
 
+def ship_placement(start_position, end_position):
+
+    # Same letters, different numbers
+    if(start_position[0] == end_position[0]):
+
+        start_number = int(start_position[1])
+        end_number = int(end_position[1])
+
+        if(start_number > end_number):
+
+            start_number, end_number = reverse_variables(start_number, end_number)
+
+        for position_number in range(start_number, end_number + 1):
+            cell = start_position[0] + str(position_number)
+            for (grid_x, grid_y), element in np.ndenumerate(grid):
+                if element == cell:
+                    grid[grid_x, grid_y] = "##"
+
+
+    # Same numbers, different letters
+    elif(start_position[1]) == end_position[1]:
+
+        start_letter = ord(start_position[0])
+        end_letter = ord(end_position[0])
+
+        if(start_letter > end_letter):
+            
+            start_letter, end_letter = reverse_variables(start_letter, end_letter)
+
+        for position_letter in range(start_letter, end_letter + 1):
+            cell = chr(position_letter) + start_position[1]
+            for (grid_x, grid_y), element in np.ndenumerate(grid):
+                if element == cell:
+                    grid[grid_x, grid_y] = "##"
+
+    else:
+        print("invalid placement")
+
+    return
