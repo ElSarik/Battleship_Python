7f4a6d97c554f034bd16e251019e2de32f174e95
ElSarik <sarikakisilias@gmail.com>
2022-03-28 10:59:33 +0300

    Updated ship_placement
    - Added a list of available ship lengths. When a ship is being placed, its length is being removed from the list.
    - Added validation for the ship length and position placement.
    - Updated ship indication on the grid.

--- grid.py
+++ grid.py
@@ -3,6 +3,9 @@ import string
 
 from calculations import reverse_variables
 
+available_ships = [2, 3, 3, 4, 5]
+
+
 def grid_init():
     global grid
 
@@ -24,40 +27,62 @@ def grid_display():
 
 def ship_placement(start_position, end_position):
 
-    # Same letters, different numbers
-    if(start_position[0] == end_position[0]):
+    global available_ships
 
-        start_number = int(start_position[1])
-        end_number = int(end_position[1])
-
-        if(start_number > end_number):
-
-            start_number, end_number = reverse_variables(start_number, end_number)
-
-        for position_number in range(start_number, end_number + 1):
-            cell = start_position[0] + str(position_number)
-            for (grid_x, grid_y), element in np.ndenumerate(grid):
-                if element == cell:
-                    grid[grid_x, grid_y] = "##"
-
-
-    # Same numbers, different letters
-    elif(start_position[1]) == end_position[1]:
-
-        start_letter = ord(start_position[0])
-        end_letter = ord(end_position[0])
-
-        if(start_letter > end_letter):
-            
-            start_letter, end_letter = reverse_variables(start_letter, end_letter)
-
-        for position_letter in range(start_letter, end_letter + 1):
-            cell = chr(position_letter) + start_position[1]
-            for (grid_x, grid_y), element in np.ndenumerate(grid):
-                if element == cell:
-                    grid[grid_x, grid_y] = "##"
+    if ((start_position not in grid) or (end_position not in grid)):
+        print("Invalid placement (start, end position)")
 
     else:
-        print("invalid placement")
+
+        # Same letters, different numbers
+        if(start_position[0] == end_position[0]):
+
+            start_number = int(start_position[1])
+            end_number = int(end_position[1])
+
+            if(start_number > end_number):
+
+                start_number, end_number = reverse_variables(start_number, end_number)
+
+            if ((end_number - start_number) + 1) not in available_ships:
+                print("Invalid ship length!")
+                
+
+            else:
+                # Replace cells with ship tile.
+                for position_number in range(start_number, end_number + 1):
+                    cell_code = start_position[0] + str(position_number)
+                    for (grid_x, grid_y), element in np.ndenumerate(grid):
+                        if element == cell_code:
+                            grid[grid_x, grid_y] = "▓▓"
+
+                available_ships.remove((end_number - start_number) + 1)
+
+
+        # Same numbers, different letters
+        elif(start_position[1]) == end_position[1]:
+
+            start_letter = ord(start_position[0])
+            end_letter = ord(end_position[0])
+
+            if(start_letter > end_letter):
+                
+                start_letter, end_letter = reverse_variables(start_letter, end_letter)
+
+            if ((end_letter - start_letter) + 1) not in available_ships:
+                print("Invalid ship length!")
+                
+
+            else:
+                for position_letter in range(start_letter, end_letter + 1):
+                    cell_code = chr(position_letter) + start_position[1]
+                    for (grid_x, grid_y), element in np.ndenumerate(grid):
+                        if element == cell_code:
+                            grid[grid_x, grid_y] = "▓▓"
+
+                available_ships.remove((end_letter - start_letter) + 1)
+
+        else:
+            print("Invalid placement (diagonal placement)")
 
     return
